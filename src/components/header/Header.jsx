7d5741a8d6b716ffefd556aeb462ebcba18e3031
Praveen Ellappan <praveenellappan@AGLS-MLT-454.local>
2022-03-02 10:26:19 +0530

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">SIMPLE</span>
+        <span className="headerTitleLg">BLOG</span>
+      </div>
+      <img
+         className="headerImg"
+        src="https://images.unsplash.com/photo-1518655048521-f130df041f66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+         alt=""
+      />
+    </div>
+  );
+}
